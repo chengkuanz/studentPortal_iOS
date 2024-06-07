32e046d09290071f98b9504d29549c0046f2a879
Chengkuan Zhao <95447572+chengkuanz@users.noreply.github.com>
2024-06-06 22:35:54 -0400

    init

---
+++ studentPortal_ios/studentPortal_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  studentPortal_ios
+//
+//  Created by chengkuan zhao on 2024-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ studentPortal_ios/studentPortal_ios/studentPortal_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  studentPortal_iosApp.swift
+//  studentPortal_ios
+//
+//  Created by chengkuan zhao on 2024-06-06.
+//
+
+import SwiftUI
+
+@main
+struct studentPortal_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
