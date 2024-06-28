9c3b5bd8da9d756d35e9f65a64b468c6329d8dc3
Chengkuan Zhao <95447572+chengkuanz@users.noreply.github.com>
2024-06-28 18:42:07 -0400

    add content preview

--- studentPortal_ios/studentPortal_ios/ContentView.swift
+++ studentPortal_ios/studentPortal_ios/ContentView.swift
@@ -1,32 +1,14 @@
+// ContentView.swift
+// studentPortal_ios
 //
-//  ContentView.swift
-//  studentPortal_ios
+// Created by chengkuan zhao on 2024-06-06.
 //
-//  Created by chengkuan zhao on 2024-06-06.
-//
-
-//import SwiftUI
-//
-//struct ContentView: View {
-//    var body: some View {
-//        Text("Hello, world!")
-//            .padding()
-//    }
-//}
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        //WebView(url: URL(string: "https://www.google.com")!)
         WebView(url: URL(string: "http://localhost:3000")!)
-    
             .edgesIgnoringSafeArea(.all)
     }
 }
