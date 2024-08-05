c1fa4519dfe0e94bfcdf500ed033aba144a235d1
Chengkuan Zhao <95447572+chengkuanz@users.noreply.github.com>
2024-08-04 23:23:57 -0400

    Update ProfileView.swift

--- studentPortal_ios/studentPortal_ios/ProfileView.swift
+++ studentPortal_ios/studentPortal_ios/ProfileView.swift
@@ -26,18 +26,6 @@ struct ProfileView: View {
                 .font(.largeTitle)
                 .padding(.top, 20)
 
-//            Button(action: {
-//                // Action to edit image
-//            }) {
-//                Text(LocalizedStringKey("EDIT IMAGE"))
-//                    .font(.headline)
-//                    .foregroundColor(.white)
-//                    .padding()
-//                    .background(Color.blue)
-//                    .cornerRadius(5.0)
-//            }
-//            .padding(.top, 10)
-//
             Image(systemName: "person.crop.circle.fill")
                 .resizable()
                 .frame(width: 100, height: 100)
